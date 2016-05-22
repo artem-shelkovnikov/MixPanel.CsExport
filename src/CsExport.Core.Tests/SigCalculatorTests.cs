a1614b99c880b3afac423f3cdf81f60f1ed1472f
Raderick <lavatroublebubble@gmail.com>
2016-05-23 00:55:26 +0500

    Added SigCalculator implementation and a simple test

---
+++ src/CsExport.Core.Tests/SigCalculatorTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace CsExport.Core.Tests
+{
+	public class SigCalculatorTests
+	{
+		private ISigCalculator _sigCalculator = new SigCalculator();
+
+		private const string Secret = "662809431a8b473862ad7814d232e0b8";
+
+		[Fact]
+		public void Calculate_When_called_with_null_as_parameters_Then_throws_argument_null_exception()
+		{
+			Assert.Throws<ArgumentNullException>(() => _sigCalculator.Calculate(null, Secret));
+		}
+
+		[Fact]
+		public void Calculate_When_called_with_null_secret_Then_throws_argument_null_exception()
+		{
+			Assert.Throws<ArgumentNullException>(() => _sigCalculator.Calculate(new Dictionary<string, string>(), null));
+		}
+
+		[Fact]
+		public void Calculate_When_called_with_test_parameters_Then_returns_expected_signature()
+		{
+			//real signature manually calculated and tested in api
+			var expectedSig = "64ccf31e71adbf89c1df33af4d28c4fa";
+
+			var parameters = GetParameters();
+			
+			var sig = _sigCalculator.Calculate(parameters, Secret);
+
+			Assert.Equal(expectedSig, sig); 
+		}
+
+		[Fact]
+		public void Calculate_When_called_with_same_parameters_in_different_order_Then_returns_same_signature()
+		{
+			var parameters = GetParameters();
+
+			var parametersOrderedByKey = parameters.OrderBy(x => x.Key).ToDictionary(x=>x.Key, x=>x.Value);
+			var parametersOrderedByKeyDescending = parameters.OrderByDescending(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+
+			var sigWithFirstOrder = _sigCalculator.Calculate(parametersOrderedByKey, Secret);
+			var sigWithSecondOrder = _sigCalculator.Calculate(parametersOrderedByKeyDescending, Secret);
+
+			Assert.Equal(sigWithFirstOrder, sigWithSecondOrder);
+			
+		}
+
+		private static Dictionary<string, string> GetParameters()
+		{
+			return new Dictionary<string, string>
+			{
+				{"expire", "1483228800"},
+				{"from_date", "2016-01-01"},
+				{"to_date", "2016-03-01"},
+				{"api_key", "ff28ac99bfb2bbc181a764684c993cd4"}
+			};
+		}
+	}
+}
---
+++ src/CsExport.Core/SigCalculator.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CsExport.Core
+{
+	public class SigCalculator : ISigCalculator
+	{
+		public string Calculate(IDictionary<string, string> parameters, string secret)
+		{
+			if (parameters == null)
+				throw new ArgumentNullException("parameters");
+
+			if (secret == null)
+				throw new ArgumentNullException("secret");
+
+			var sortedParameters = parameters.OrderBy(x => x.Key);
+
+			var stringToHash = string.Format("{0}{1}", string.Join(string.Empty, sortedParameters.Select(x=> string.Format("{0}={1}", x.Key, x.Value))), secret);
+
+			var calculatedHash = CalculateMD5(stringToHash);
+
+			return calculatedHash.ToLowerInvariant();
+		}
+
+		private string CalculateMD5(string stringToHash)
+		{
+			var md5 = MD5.Create();
+
+			var bytesToHash = Encoding.ASCII.GetBytes(stringToHash);
+
+			var computedHashBytes = md5.ComputeHash(bytesToHash);
+
+			StringBuilder sb = new StringBuilder();
+			foreach (byte hashByte in computedHashBytes)
+			{
+				sb.Append(hashByte.ToString("X2"));
+			}
+			return sb.ToString();
+		}
+	}
+}
