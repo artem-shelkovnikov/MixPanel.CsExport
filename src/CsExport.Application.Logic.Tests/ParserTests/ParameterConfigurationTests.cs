aeb253937722fa70f7e446db6e244cdd08601c2a
Raderick <lavatroublebubble@gmail.com>
2016-11-01 21:14:19 +0500

    Added validation to parameter configuration

--- src/CsExport.Application.Logic.Tests/ParserTests/ParameterConfigurationTests.cs
+++ src/CsExport.Application.Logic.Tests/ParserTests/ParameterConfigurationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using CsExport.Application.Logic.Parser;
 using Xunit;
 
@@ -16,6 +17,94 @@ namespace CsExport.Application.Logic.Tests.ParserTests
 		public void Ctor_When_called_for_parameterDefinition_without_propertyInfo_Then_throws()
 		{
 			Assert.Throws<ArgumentNullException>(() => new ParameterConfiguration(new ParameterDefinition()));
+		}
+
+		[Fact]
+		public void WithSignature_When_called_for_lowercase_alзha_signature_Then_sets_parameterDefinition_signature()
+		{
+			var signature = "signature";
+			var parameterDefinition = GetValidParameterDefinition();
+
+			var parameterConfiguration =
+				new ParameterConfiguration(parameterDefinition);
+
+			parameterConfiguration.WithSignature(signature);
+
+			Assert.Equal(signature, parameterDefinition.Signature);
+		}
+
+		[Fact]
+		public void WithSignature_When_called_for_lowercase_alpha_signature_with_dashes_Then_sets_parameterDefinition_signature()
+		{
+			var signature = "signature-with-dash";
+			var parameterDefinition = GetValidParameterDefinition();
+
+			var parameterConfiguration =
+				new ParameterConfiguration(parameterDefinition);
+
+			parameterConfiguration.WithSignature(signature);
+
+			Assert.Equal(signature, parameterDefinition.Signature);
+		}
+
+		[Fact]
+		public void WithSignature_When_called_for_signature_containing_space_Then_throws_argumentException()
+		{
+			var signature = "signature with space";
+			var parameterDefinition = GetValidParameterDefinition();
+
+			var parameterConfiguration =
+				new ParameterConfiguration(parameterDefinition);
+
+			Assert.Throws<ArgumentException>(() => parameterConfiguration.WithSignature(signature));
+		}
+
+		[Fact]
+		public void WithSignature_When_called_for_signature_containing_special_character_Then_throws_argumentException()
+		{
+			var signature = "signature#with#hashtags";
+			var parameterDefinition = GetValidParameterDefinition();
+
+			var parameterConfiguration =
+				new ParameterConfiguration(parameterDefinition);
+
+			Assert.Throws<ArgumentException>(() => parameterConfiguration.WithSignature(signature));
+		}
+
+		[Fact]
+		public void WithSignature_When_called_for_too_long_signature_Then_throws_argumentException()
+		{
+			var signature = "too-long-parameter-signature-with-too-many-letters-and-words";
+			var parameterDefinition = GetValidParameterDefinition();
+
+			var parameterConfiguration =
+				new ParameterConfiguration(parameterDefinition);
+
+			Assert.Throws<ArgumentException>(() => parameterConfiguration.WithSignature(signature));
+		}
+
+		[Fact]
+		public void WithSignature_When_called_for_null_Then_throws_argumentNullException()
+		{											  
+			var parameterDefinition = GetValidParameterDefinition();
+
+			var parameterConfiguration =
+				new ParameterConfiguration(parameterDefinition);
+
+			Assert.Throws<ArgumentNullException>(() => parameterConfiguration.WithSignature(null));
+		}
+
+		private static ParameterDefinition GetValidParameterDefinition()
+		{
+			return new ParameterDefinition
+			{
+				PropertyInfo = typeof(StubClass).GetProperty(nameof(StubClass.StringProperty))
+			};
+		}
+
+		private class StubClass
+		{
+			public string StringProperty { get; set; }
 		}
 	}
 }
--- src/CsExport.Application.Logic/Parser/ParameterConfiguration.cs
+++ src/CsExport.Application.Logic/Parser/ParameterConfiguration.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Text.RegularExpressions;
 
 namespace CsExport.Application.Logic.Parser
 {
 	public class ParameterConfiguration
 	{
+		private const int MaxSignatureLength = 30;
 		private readonly ParameterDefinition _parameterDefinition;
+		private static Regex _signatureValidationRegex = new Regex("^[a-z0-9-]+$");
 
 		internal ParameterConfiguration(ParameterDefinition parameterDefinition)
 		{
@@ -21,6 +24,12 @@ namespace CsExport.Application.Logic.Parser
 		{
 			if (signature == null)
 				throw new ArgumentNullException(nameof(signature));
+
+			if (_signatureValidationRegex.Match(signature).Value != signature)
+				throw new ArgumentException("Parameter signature must contain only lower case letter, numbers and dashes", nameof(signature));
+
+			if (signature.Length > MaxSignatureLength)
+				throw new ArgumentException($"Signature maximum length is {MaxSignatureLength}", nameof(signature));
 
 			_parameterDefinition.Signature = signature;
 
